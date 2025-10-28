007f70cbc8ee2481353fc8e095c81b747c16190b
Mark <jiayouzl@2980.com>
2025-10-28 14:52:10 +0800

    Initial Commit

---
+++ test1/test1App.swift
@@ -0,0 +1,17 @@
+//
+//  test1App.swift
+//  test1
+//
+//  Created by zl_vm on 2025/10/28.
+//
+
+import SwiftUI
+
+@main
+struct test1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
